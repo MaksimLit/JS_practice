1c03779387fba6fdbcbc9f106636e22c4ecc5aff
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-06-07 18:43:23 +0300

    Создал функцию createModal

--- plugins/modal/molal.js
+++ plugins/modal/molal.js
@@ -1,3 +1,42 @@
-$.modal = function () {
-    
-}
+function _createModal (options) {
+    const modal = document.createElement( 'div');
+    modal.classList.add('mlmodal');
+    modal.insertAdjacentHTML('afterbegin', `
+    <div class="modal__overlay">
+        <div class="modal__window">
+            <div class="modal__header">
+                <span class="modal__title">Modal title</span>
+                <span class="modal__close">&times;</span>
+            </div>
+            <div class="modal__body">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="modal__footer">
+                <button>Хорошо</button>
+                <button>Отмена</button>
+            </div>
+        </div>
+    </div>
+    `)
+    document.body.appendChild(modal);
+    return modal;
+};
+
+$.modal = function (options) {
+    const $modal = _createModal(options);
+
+    return {
+        open() {
+
+        },
+
+        close() {
+
+        },
+
+        destroy() {
+
+        }
+    }
+};
